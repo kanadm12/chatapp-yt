1762c4518d7422962cac16e6951981b235941cf6
kanadm12 <kanadmukherjee12@gmail.com>
2025-02-24 15:54:42 +0530

    changed tailwindconfig.js

--- Backend/Frontend/tailwind.config.js
+++ Backend/Frontend/tailwind.config.js
@@ -1,8 +1,11 @@
+// Use ESM import syntax
+import daisyui from "daisyui";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [require("daisyui")],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui], // Use imported daisyui
 };
